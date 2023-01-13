2ce910fc21d495119cc71aced5b6118a775d1561
poornimarama93 <poornimarama@gmail.com>
2023-01-13 11:13:10 +0530

    Initial commit

---
+++ uimodule/webapp/controller/Products.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define(
+    ["./BaseController"],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("tutorial.products.controller.Products", {
+            handleListItemPress: function (oEvent) {
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                var selectedProductId = oEvent.getSource().getBindingContext().getProperty("ProductID");
+                oRouter.navTo("ProductDetail", {
+                  productId: selectedProductId
+                });
+              }
+        });
+    }
+);
